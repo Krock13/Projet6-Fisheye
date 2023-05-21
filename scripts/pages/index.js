82340a96036a90684ccee549eded3091f5d25966
krock13 <krock.airdrop@hotmail.com>
2023-05-21 15:40:04 +0200

    Dynamic display of the photographer's card in photographer.html

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,22 +1,3 @@
-async function getPhotographers() {
-	try {
-		// Fetch the photographers data from the specified JSON file
-		const response = await fetch('./data/photographers.json');
-		if (!response.ok) {
-			throw new Error('Failed to fetch photographers data');
-		}
-		// Parse the response data as JSON
-		const data = await response.json();
-		// Return the photographers data
-		return { photographers: data.photographers };
-	} catch (error) {
-		// Handle any errors that occur during the fetch
-		console.error(error);
-		// Return an empty array if there's an error
-		return { photographers: [] };
-	}
-}
-
 async function displayData(photographers) {
 	const photographersSection = document.querySelector('.photographer_section');
 
