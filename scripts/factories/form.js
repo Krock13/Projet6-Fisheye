cd1bca42a54a94a28425b65d79841c18ad534785
krock13 <krock.airdrop@hotmail.com>
2023-06-03 14:14:48 +0200

    Make the form navigable by keyboard

--- scripts/factories/form.js
+++ scripts/factories/form.js
@@ -3,9 +3,11 @@ import { closeModal } from '../utils/contactForm.js';
 export function createModalDOM() {
 	const name = document.querySelector('.photographer_name');
 
+	// Create modal container
 	const modal = document.createElement('div');
 	modal.classList.add('modal');
 
+	// Create header section
 	const header = document.createElement('header');
 	const title = document.createElement('h2');
 	const titleId = 'modal-title';
@@ -19,9 +21,10 @@ export function createModalDOM() {
 	header.appendChild(title);
 	header.appendChild(closeIcon);
 
+	// Create form section
 	const form = document.createElement('form');
 
-	// Créer le champ Prénom
+	// Create First Name input field
 	const firstNameLabel = document.createElement('label');
 	firstNameLabel.textContent = 'Prénom';
 	const firstNameInput = document.createElement('input');
@@ -33,7 +36,7 @@ export function createModalDOM() {
 	form.appendChild(firstNameLabel);
 	form.appendChild(firstNameInput);
 
-	// Créer le champ Nom
+	// Create Last Name input field
 	const lastNameLabel = document.createElement('label');
 	lastNameLabel.textContent = 'Nom';
 	const lastNameInput = document.createElement('input');
@@ -45,7 +48,7 @@ export function createModalDOM() {
 	form.appendChild(lastNameLabel);
 	form.appendChild(lastNameInput);
 
-	// Créer le champ Email
+	// Create Email input field
 	const emailLabel = document.createElement('label');
 	emailLabel.textContent = 'Email';
 	const emailInput = document.createElement('input');
@@ -57,7 +60,7 @@ export function createModalDOM() {
 	form.appendChild(emailLabel);
 	form.appendChild(emailInput);
 
-	// Créer le champ Votre message
+	// Create Message input field
 	const messageLabel = document.createElement('label');
 	messageLabel.textContent = 'Votre message';
 	const messageInput = document.createElement('textarea');
@@ -68,67 +71,39 @@ export function createModalDOM() {
 	form.appendChild(messageLabel);
 	form.appendChild(messageInput);
 
-	// Créer le bouton Envoyer
+	// Create Submit button
 	const submitButton = document.createElement('button');
 	submitButton.textContent = 'Envoyer';
 	submitButton.type = 'submit';
 	form.appendChild(submitButton);
 
-	// Gestionnaire d'événement lors du clic sur le bouton Envoyer
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
 
-		// Vérification des champs avec les expressions régulières
-		const regexFirstName = /^[a-zA-Zéèàùç'-]+$/;
-		const regexLastName = /^[a-zA-Zéèàùç'-]+$/;
-		const regexEmail = /^\S+@\S+\.\S+$/;
-
-		// Récupération des valeurs des champs
+		// Retrieve input values
 		const firstNameValue = firstNameInput.value.trim();
 		const lastNameValue = lastNameInput.value.trim();
 		const emailValue = emailInput.value.trim();
 		const messageValue = messageInput.value.trim();
 
-		let isValid = true;
+		// Log input values
+		console.log('Prénom:', firstNameValue);
+		console.log('Nom:', lastNameValue);
+		console.log('Email:', emailValue);
+		console.log('Votre message:', messageValue);
 
-		// Vérification du champ Prénom
-		if (!regexFirstName.test(firstNameValue)) {
-			isValid = false;
-		}
-
-		// Vérification du champ Nom
-		if (!regexLastName.test(lastNameValue)) {
-			isValid = false;
-		}
-
-		// Vérification du champ Email
-		if (!regexEmail.test(emailValue)) {
-			isValid = false;
-		}
-
-		// Vérification du champ Votre message
-		if (messageValue === '') {
-			isValid = false;
-		}
-
-		if (isValid) {
-			// Les champs sont tous valides, affichage des valeurs dans la console
-			console.log('Prénom:', firstNameValue);
-			console.log('Nom:', lastNameValue);
-			console.log('Email:', emailValue);
-			console.log('Votre message:', messageValue);
-
-			// Réinitialisation des champs
-			firstNameInput.value = '';
-			lastNameInput.value = '';
-			emailInput.value = '';
-			messageInput.value = '';
-		}
+		// Clear input fields
+		firstNameInput.value = '';
+		lastNameInput.value = '';
+		emailInput.value = '';
+		messageInput.value = '';
 	});
 
+	// Append header and form to the modal
 	modal.appendChild(header);
 	modal.appendChild(form);
 
+	// Set attributes for accessibility
 	modal.setAttribute('id', 'modal');
 	modal.setAttribute('aria-labelledby', titleId);
 
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,12 +3,20 @@ import { createModalDOM } from '../factories/form.js';
 export function displayModal() {
 	const main = document.querySelector('#main');
 	const modal = document.getElementById('contact_modal');
+
+	// Create modal DOM if it doesn't exist
 	if (!modal.hasChildNodes()) {
 		const modalDOM = createModalDOM();
 		modal.appendChild(modalDOM);
 	}
 	modal.style.display = 'block';
 	main.style.opacity = 0.6;
+
+	// Set focus on the first input element in the modal
+	const firstInputElement = modal.querySelector('input');
+	if (firstInputElement) {
+		firstInputElement.focus();
+	}
 }
 
 export function closeModal() {
@@ -16,10 +24,17 @@ export function closeModal() {
 	const modal = document.getElementById('contact_modal');
 	modal.style.display = 'none';
 	main.style.opacity = 1;
+
+	// Set focus back to the element that opened the modal
+	const openerElement = document.querySelector('.contact_button');
+	openerElement.focus();
 }
 
 function closeModalOnEscape(event) {
-	if (event.key === 'Escape') {
+	const modal = document.getElementById('contact_modal');
+
+	// Close modal on Escape key press only if the modal is currently open
+	if (event.key === 'Escape' && modal.style.display === 'block') {
 		closeModal();
 	}
 }
